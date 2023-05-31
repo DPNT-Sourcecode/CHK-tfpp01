b0903fb2cdb8d2f950376933254c35bbc04fb334
sbx_user1051 <sbx_user1051@169.254.12.93>
2023-05-31 22:18:59 +0000

    Wed May 31 22:18:59 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,36 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    total = 0
+    cnt_A = 0
+    cnt_B = 0
+
+    for sku in skus:
+        
+        if sku not in 'ABCD':
+            return -1
+
+        elif sku == 'A':
+            cnt_A += 1
+
+        elif sku == 'B':
+            cnt_B += 1
+        
+        elif sku == 'C':
+            total += 20
+
+        elif sku == 'D':
+            total += 15
+
+    # Get groups of multipack and remainder
+    grp_A = cnt_A // 3
+    rem_A = cnt_A % 3
+
+    grp_B = cnt_B // 2
+    rem_B = cnt_B % 2
+
+    total += (grp_A*130) + (rem_A*50) + (grp_B*45) + (rem_B*30)
+
+    return total
+
+
